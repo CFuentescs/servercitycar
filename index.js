67d8a6cef54c49f78c85076f0066fa020af90e61
carlos <c.fuentescorts@gmail.com>
2019-04-07 15:46:30 -0700

    base de dato prueba 3

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ express()
    	  let pass = req.body.pass;
    	  console.log('holi'+email);
       const client = await pool.connect()
-      const result = await client.query(`SELECT * FROM login where ${email} = email AND ${pass} = pass`);
+      const result = await client.query(`SELECT * FROM login`);
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
