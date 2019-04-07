4c8d8f0d9f9cbd8436536453f1322b633ca603a9
carlos <c.fuentescorts@gmail.com>
2019-04-07 15:05:47 -0700

    base de dato prueba

--- index.js
+++ index.js
@@ -15,10 +15,25 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/db', async (req, res) => {
+  /*.get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM login');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })*/
+  .post('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      var email = req.param('email');
+      var pass = req.param('pass');
+      const result = await client.query(`SELECT * FROM login where ${ email} LIKE email AND ${pass} LIKE pass`);
+
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
