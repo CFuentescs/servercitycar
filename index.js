9cc2dd0bfd928f77b593cbea763b4019a33ca420
carlos <c.fuentescorts@gmail.com>
2019-04-07 16:25:12 -0700

    base de dato prueba 6

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ express()
   .use(bodyParser.urlencoded({extended:false}))
   .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
   .post('/db', async (req, res) => {
     try {
    	  const email = req.param("email");
@@ -30,20 +31,5 @@ express()
       console.error(err);
       res.send("Error " + err);
     }
-   }) 
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  /*.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  }) */ 
-  
+  }) 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
